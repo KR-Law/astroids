d142a5112ffeace5ce2197f334715dafd4f43801
Kyle Law <dev@anzenketh.net>
2025-06-29 22:27:25 -0600

    Add Window

--- main.py
+++ main.py
@@ -10,10 +10,13 @@ def main():
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
   while True:
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+          return
     screen.fill("black")
 
-  # At End for Refresh
-  pygame.display.flip()
+    # At End for Refresh
+    pygame.display.flip()
 
 if __name__ == "__main__":
   main()
