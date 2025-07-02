d24011554663596e14958cec72f2d11eae2d5fee
Kyle Law <dev@anzenketh.net>
2025-07-01 19:48:44 -0600

    CH2L8 - Add Groups

--- main.py
+++ main.py
@@ -5,20 +5,30 @@ from player import Player
 
 def main():
     pygame.init()
-    clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        updatable.update(dt)
 
         screen.fill("black")
+     
+        for sprite in drawable:
+            sprite.draw(screen)
 
-        player.update(dt)
-        player.draw(screen)
         pygame.display.flip()
 
         # Limit the framerate to 60 FPS
