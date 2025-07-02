7ed3c954b7acc56e6e3e37b703a296f8333b0729
Kyle Law <dev@anzenketh.net>
2025-07-01 19:36:13 -0600

    CH2L7 - Created Player

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 import pygame
-from constants import *
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
 
 
 def main():
-  print("Starting Asteroids!")
-  print(f"Screen width: {SCREEN_WIDTH}")
-  print(f"Screen height: {SCREEN_HEIGHT}")
-  pygame.init()
-  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-  while True:
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-          return
-    screen.fill("black")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
-    # At End for Refresh
-    pygame.display.flip()
+        screen.fill("black")
+
+        player.update(dt)
+        player.draw(screen)
+        pygame.display.flip()
+
+        # Limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
-  main()
+    main()
